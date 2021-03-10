6e2383a6eab32f9bd9f337a25821e74f9a64739d
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-03-10 17:10:33 +0100

    ADD inquirer

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Hola mundo');
+const { leerInput, inquirerMenu, pause } = require('./helpers/inquirer.js');
+
+const main = async () => {
+  let opt;
+
+  do {
+    opt = await inquirerMenu();
+    switch (opt) {
+      case 1:
+        console.log('Buscar ciudad');
+        break;
+      case 2:
+        console.log('Historial');
+        break;
+      case 0:
+        console.log('Salir');
+        break;
+    }
+    await pause();
+  } while (opt !== 0);
+};
+
+main();
