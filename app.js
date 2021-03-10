410b781498aadf65c3b23edd757513fe74eac49e
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-03-10 18:35:47 +0100

    ADD axios call

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
 const { leerInput, inquirerMenu, pause } = require('./helpers/inquirer.js');
+const Busquedas = require('./models/busquedas');
 
 const main = async () => {
+  const busquedas = new Busquedas();
   let opt;
 
   do {
     opt = await inquirerMenu();
     switch (opt) {
       case 1:
-        console.log('Buscar ciudad');
+        const lugar = await leerInput('Ciudad: ');
+        await busquedas.ciudad(lugar);
+
+        console.log('\n Información de la ciudad\n'.green);
+        console.log('Ciudad:');
+        console.log('Lat:');
+        console.log('Lng:');
+        console.log('Temperatura:');
+        console.log('Mínima:');
+        console.log('Máxima:');
         break;
       case 2:
         console.log('Historial');
---
+++ models/busquedas.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+class Busquedas {
+  constructor() {}
+
+  get paramsMapbox() {
+    return {
+      access_token: process.env.MAPBOX_KEY,
+      limit: 5,
+      language: 'es',
+    };
+  }
+
+  async ciudad(lugar = '') {
+    try {
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapbox,
+      });
+      const resp = await instance.get();
+      console.log(resp.data);
+      return [];
+    } catch (e) {
+      return [];
+    }
+  }
+}
+
+module.exports = Busquedas;
