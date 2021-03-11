1a344b41f416eff337d094a6fa874f08a1561246
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-03-11 18:29:47 +0100

    ADD data to json

--- app.js
+++ app.js
@@ -18,18 +18,27 @@ const main = async () => {
         const termino = await leerInput('Ciudad: ');
         const lugares = await busquedas.ciudad(termino);
         const id = await listarLugares(lugares);
-        const lugarSel = lugares.find((l) => l.id === id);
+        if (id === '0') continue;
 
+        const lugarSel = lugares.find((l) => l.id === id);
+        busquedas.agregarHistorial(lugarSel.nombre);
+        const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
+
+        console.clear();
         console.log('\n Información de la ciudad\n'.green);
         console.log('Ciudad: ', lugarSel.nombre);
         console.log('Lat: ', lugarSel.lat);
         console.log('Lng: ', lugarSel.lng);
-        console.log('Temperatura:');
-        console.log('Mínima:');
-        console.log('Máxima:');
+        console.log('Temperatura: ', clima.temp);
+        console.log('Mínima: ', clima.min);
+        console.log('Máxima: ', clima.max);
+        console.log('Descripción: ', clima.desc);
         break;
       case 2:
-        console.log('Historial');
+        busquedas.historial.forEach((lugar, i) => {
+          const idx = `${i + 1}.`.green;
+          console.log(`${idx} ${lugar}`);
+        });
         break;
       case 0:
         console.log('Salir');
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
 const axios = require('axios');
 
 class Busquedas {
+  historial = [];
+  dbPath = './db/database.json';
+
   constructor() {}
 
   get paramsMapbox() {
@@ -28,6 +32,50 @@ class Busquedas {
       return [];
     }
   }
+
+  get paramsWeather() {
+    return {
+      appid: process.env.OPENWEATHER_KEY,
+      lang: 'es',
+      units: 'metric',
+    };
+  }
+
+  async climaLugar(lat, lon) {
+    try {
+      const instance = axios.create({
+        baseURL: 'https://api.openweathermap.org/data/2.5/weather',
+        params: { ...this.paramsWeather, lat, lon },
+      });
+      const resp = await instance.get();
+      const { weather, main } = resp.data;
+      return {
+        desc: weather[0].description,
+        min: main.temp_min,
+        max: main.temp_max,
+        temp: main.temp,
+      };
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  agregarHistorial(lugar = '') {
+    if (this.historial.includes(lugar.toLowerCase())) {
+      return;
+    }
+    this.historial.unshift(lugar.toLowerCase());
+    this.guardarDB();
+  }
+
+  guardarDB() {
+    const payload = {
+      historial: this.historial,
+    };
+    fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+  }
+
+  leerDB() {}
 }
 
 module.exports = Busquedas;
